98b084cec35fe4f9b70d1039ecc69419a06aab85
AtmoOmen <atmoomen2004@outlook.com>
2026-05-20 13:18:38 +0800

    调整布局

--- XIVUIColorPreviewer/MainWindow.xaml.cs
+++ XIVUIColorPreviewer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 using Windows.UI;
 using Microsoft.Extensions.Logging;
 using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
